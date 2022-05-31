7386fc3a0aa432cd1c67ea98b0bc382cb2e03984
Adria Costa <adriacosta@bemol.com.br>
2022-05-31 10:51:39 -0400

    acessando banco de dados

---
+++ exemplosMongodb/acessandoMongodb.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace exemplosMongodb
+{
+    class acessandoMongodb
+    {
+        static void Main(string[] args)
+        {
+            Task T = MainSync(args);
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+
+        static async Task MainSync(string[] args)
+        {
+            var doc = new BsonDocument
+            {
+                {"Título", "Guerra dos Tronos"},
+                {"Autor", "George R R Martin"},
+                {"Ano", "1999"},
+                {"Páginas", "856"}
+            };
+
+            var assuntoArray = new BsonArray();
+            assuntoArray.Add("Fantasia");
+            assuntoArray.Add("Ação");
+            doc.Add("Assunto", assuntoArray);
+
+            Console.WriteLine(doc);
+
+            //acesso ao servidor Mongodb
+            string strConexao = "mongodb://localhost:27017";
+            IMongoClient cliente = new MongoClient(strConexao);
+
+            //acesso ao banco de dados
+            IMongoDatabase bancoDados = cliente.GetDatabase("Biblioteca");
+
+            //acesso colecao
+            IMongoCollection<BsonDocument> coelcao = bancoDados.GetCollection<BsonDocument>("Livros");
+
+
+        }
+    }
+}
