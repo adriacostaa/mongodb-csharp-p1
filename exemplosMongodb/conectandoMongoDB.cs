778eafb67a930631c04a0926d92a81516e610b9f
Adria Costa <adriacosta@bemol.com.br>
2022-06-20 17:43:24 -0400

    conectando mongodb

---
+++ exemplosMongodb/conectandoMongoDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace exemplosMongodb
+{
+    class conectandoMongoDB
+    {
+        public const string STRCONEXAO = "mongodb://localhost:27017";
+        public const string NOME_BASE = "Biblioteca";
+        public const string COLECAO = "Livros";
+
+        private static IMongoClient _cliente;
+        private static IMongoDatabase _BaseDeDados;
+
+        static conectandoMongoDB()
+        {
+            _cliente = new MongoClient(STRCONEXAO);
+            _BaseDeDados = _cliente.GetDatabase(NOME_BASE);
+        }
+
+    }
+}
